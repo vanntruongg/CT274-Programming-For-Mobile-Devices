cd5febea107888ce22cd5bf114322301845978a2
Van Truong <truongtran.lv2019@gmail.com>
2023-10-20 12:23:24 +0700

    add project QuizApp

---
+++ B2012051_QuizApp/settings.gradle.kts
@@ -0,0 +1,18 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "B2012051_QuizApp"
+include(":app")
+ 
